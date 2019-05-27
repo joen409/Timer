289f536c9c08e81d0867395aaaeb7085c186f805
Youngseo Shin <joen50838@gmail.com>
2019-05-27 09:03:27 +0900

    #2 타이머 동작 코드 구현

--- Timer/Form1.cs
+++ Timer/Form1.cs
@@ -7,18 +7,74 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Microsoft.VisualBasic;
+
 namespace Timer
 {
     public partial class Form1 : Form
     {
+        int CountOrgNum = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void BtnCount_Click(object sender, EventArgs e)
         {
+            ProcessTimer();
+        }
 
+        private void ProcessTimer()
+        {
+            if (IntCheck())
+            {
+                this.CountOrgNum = Convert.ToInt32(this.txtNum.Text);
+                this.txtNum.ReadOnly = true;
+                this.Timer.Enabled = true;
+            }
+            else
+            {
+                this.txtNum.Focus();
+                this.txtNum.Text = "";
+            }
+        }
+
+        private bool IntCheck()
+        {
+            if (Information.IsNumeric(this.txtNum.Text))
+            {
+                return true;
+
+            }
+            else
+            {
+                MessageBox.Show("숫자만 입력해주세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (this.CountOrgNum < 1)
+            {
+                this.Timer.Enabled = false;
+                MessageBox.Show("펑!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtNum.ReadOnly = false;
+                this.txtNum.Focus();
+            }
+            else
+            {
+                this.CountOrgNum = this.CountOrgNum - 1;
+                this.txtCountDown.Text = Convert.ToString(this.CountOrgNum);
+            }
+        }
+
+        private void BtnCount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                ProcessTimer();
+            }
         }
     }
 }
